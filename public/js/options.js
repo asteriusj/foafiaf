558ef7881b61363bfa1f2da4dda418e893addef2
Laura Cserep <lauracserep@hotmail.com>
2016-09-01 11:28:31 -0500

    Cleanup.  Put styles where they belong.

--- public/js/options.js
+++ public/js/options.js
@@ -1,233 +1,236 @@
 
       function  getOptions() {
       
-      return {
-        
-        physics: {
-          solver: 'forceAtlas2Based',
-          maxVelocity: 10,
-          minVelocity: 1,
-        },
-        
-        
-        groups: {
-          property: {
-            shape: 
-              'dot'
-            ,
-            size: 18,
-            color: 'pink'
-          },
-          
-          group: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf0c0',
-              size: 50,
-              color: '#57169a'
-            }
-          },
-          
-          team: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf007',
-              size: 50,
-              color: 'grey'
-            }
-          },
-          person: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf007',
-              size: 50,
-              color: '#aa00ff'
-            }
-          },
-          
-          role: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf21d',
-              size: 50,
-              color: '#6E6EFD'
-            }
-          },
-       
-       
-          organization: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf0e8',
-              size: 50,
-              color: 'blue'
-            }
-          },   
-          orgbiz: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf069',
-              size: 50,
-              color: '#57169a'
-            }
-          },
-          
-          orggov: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf19c',
-              size: 50,
-              color: 'blue'
-            }
-          },
-          
-          orgnonprofit: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf069',
-              size: 50,
-              color: 'green'
-            }
-          },
-          
-          
-          orgfaith: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf004',
-              size: 50,
-              color: 'cyan'
-            }
-          },
-          
-          globe: {
-            shape: 'icon',
-            icon: {
-              face: 'Ionicons',
-              code: '\uf276',
-              size: 66,
-              color: '#6E6EFD'
-            }
-          },
-          
-          marker: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf041',
-              size: 50,
-              color: '#FB7E81'
-            }
-          },
-          
-          book: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf02d',
-              size: 50,
-              color: '#C2FABC'
-            }
-          },
-          film: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf008',
-              size: 55,
-              color: '#6E6EFD'
-            }
-          },
-          
-          
-          tags: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf02c',
-              size: 40,
-              color: 'lime'
-            }
-          },
-          tag: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf02b',
-              size: 40,
-              color: 'lime'
-            }
-          },
-          
-          projects: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf0ae',
-              size: 44,
-              color: '#800000'
-            }
-          },
-          project: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf03a',
-              size: 40,
-              color: 'maroon'
-            }
-          },
-          
-          bullseye: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf140',
-              size: 40,
-              color: 'red'
-            }
-          },
-          puzzle: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf12e',
-              size: 40,
-              color: 'yellow'
-            }
-          },
-          
-          cubes: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf1b3',
-              size: 44,
-              color: 'black'
-            }
-          },
-          cube: {
-            shape: 'icon',
-            icon: {
-              face: 'FontAwesome',
-              code: '\uf1b2',
-              size: 40,
-              color: 'black'
-            }
-          },
+		  return {
+			
+			layout: {
+				randomSeed: 10,
+				improvedLayout:false
+			},
 
-          
-        }          
-        
-      }
+
+			physics: {
+			  solver: 'forceAtlas2Based',
+			  maxVelocity: 10,
+			  minVelocity: 1,
+			},
+
+
+			groups: {
+			  property: {
+				shape: 
+				  'dot'
+				,
+				size: 18,
+				color: 'pink'
+			  },
+			  
+			  group: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf0c0',
+				  size: 50,
+				  color: '#57169a'
+				}
+			  },
+			  
+			  team: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf007',
+				  size: 50,
+				  color: 'grey'
+				}
+			  },
+			  person: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf007',
+				  size: 50,
+				  color: '#aa00ff'
+				}
+			  },
+			  
+			  role: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf21d',
+				  size: 50,
+				  color: '#6E6EFD'
+				}
+			  },
+
+
+			  organization: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf0e8',
+				  size: 50,
+				  color: 'blue'
+				}
+			  },   
+			  orgbiz: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf069',
+				  size: 50,
+				  color: '#57169a'
+				}
+			  },
+			  
+			  orggov: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf19c',
+				  size: 50,
+				  color: 'blue'
+				}
+			  },
+			  
+			  orgnonprofit: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf069',
+				  size: 50,
+				  color: 'green'
+				}
+			  },
+			  
+			  
+			  orgfaith: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf004',
+				  size: 50,
+				  color: 'cyan'
+				}
+			  },
+			  
+			  globe: {
+				shape: 'icon',
+				icon: {
+				  face: 'Ionicons',
+				  code: '\uf276',
+				  size: 66,
+				  color: '#6E6EFD'
+				}
+			  },
+			  
+			  marker: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf041',
+				  size: 50,
+				  color: '#FB7E81'
+				}
+			  },
+			  
+			  book: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf02d',
+				  size: 50,
+				  color: '#C2FABC'
+				}
+			  },
+			  film: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf008',
+				  size: 55,
+				  color: '#6E6EFD'
+				}
+			  },
+			  
+			  
+			  tags: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf02c',
+				  size: 40,
+				  color: 'lime'
+				}
+			  },
+			  tag: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf02b',
+				  size: 40,
+				  color: 'lime'
+				}
+			  },
+			  
+			  projects: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf0ae',
+				  size: 44,
+				  color: '#800000'
+				}
+			  },
+			  project: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf03a',
+				  size: 40,
+				  color: 'maroon'
+				}
+			  },
+			  
+			  bullseye: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf140',
+				  size: 40,
+				  color: 'red'
+				}
+			  },
+			  puzzle: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf12e',
+				  size: 40,
+				  color: 'yellow'
+				}
+			  },
+			  
+			  cubes: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf1b3',
+				  size: 44,
+				  color: 'black'
+				}
+			  },
+			  cube: {
+				shape: 'icon',
+				icon: {
+				  face: 'FontAwesome',
+				  code: '\uf1b2',
+				  size: 40,
+				  color: 'black'
+				}
+			  },
+			}        
+		}
       
     };
