a0c63130757692825aca9cf3fd7d482d4fcbce9d
asteriusj <jstewart@asteriusmedia.com>
2017-04-05 15:24:10 -0500

    Update timeline.js

--- public/timelines/timeline.js
+++ public/timelines/timeline.js
@@ -43,30 +43,94 @@ function buildDataSet(graph, cb) {
 
 	  
     let myDataSets = []
+    let myItems = []
+    let myGroups = []
     
     //console.log('list', list)
     for (let i=0; i<list.length; i++) {
         let _parentid = "foafiaf:" + list[i] ;
         let _id = list[i] ;
-        let _name = list[i].replace('_',' ') ;
+        if (_id) _id = _id.replace('&', '') ;
+        let _name = list[i].replace('Spoke_',' ') ;
+        
+        // prep group
+        let group = {}
+        group.id = _id;
+        group.content = _name;
+        console.log('group', group)
+        myGroups.push(group);
+        
+        // prep dataset
         let measureNodes = getNodesByParent(graph, 'foafiaf:Measure', 'foafiaf:Spoke', _parentid)
         let dataset = prepNodes(measureNodes);
         
         if (dataset.length) {
             // add div with h3 tags
-            var droot = setDiv(_id, _name)
+              //var droot = setDiv(_id, _name)                  // COMMENT OUT DIVS
             
             // var froot = setCbx(_id, _name)
             
-            var tline = setTimeline(_id, dataset)
+              //var tline = setTimeline(_id, dataset)         //comment out individual timelines
 
             myDataSets.push(dataset);
+            
+            for (let j = 0; j<dataset.length; j++) {
+                myItems.push(dataset[j])
+            }
+
         }
     }
-      
+    let group = {}
+    group.id = '0Period';
+    group.content = 'Period';
+    myGroups.push(group)
+    
+    setGroupTimeline(myGroups, myItems)
     //console.log('myDataSets', myDataSets)
     //console.log('myDataSets', JSON.stringify(myDataSets))
     if (cb) cb(null, myDataSets)
+}
+
+function setGroupTimeline(groups, items){
+    console.log('setTimeline')
+    
+    
+    items.push({id: '2017', content: '2017', group: '0Period', start: '2017-01-01', end: '2017-12-31', type: 'background', className: 'cls2017' }) ;
+    items.push({id: '2018', content: '2018' ,group: '0Period', start: '2018-01-01', end: '2018-12-31', type: 'background', className: 'cls2018 negative'}) ;
+    items.push({id: '2019', content: '2019', group: '0Period', start: '2019-01-01', end: '2019-12-31', type: 'background', className: 'cls2019' }) ;
+    items.push({id: '2020', content: '2020', group: '0Period', start: '2020-01-01', end: '2020-12-31', type: 'background', className: 'cls2020 negative'}) ;
+    items.push({id: '2021', content: '2021', group: '0Period', start: '2021-01-01', end: '2021-12-31', type: 'background', className: 'cls2021' }) ;
+    items.push({id: '2022', content: '2022', group: '0Period', start: '2022-01-01', end: '2022-12-31', type: 'background', className: 'cls2022 negative'}) ;
+    items.push({id: '2023', content: '2023', group: '0Period', start: '2023-01-01', end: '2023-12-31', type: 'background', className: 'cls2023' }) ;
+    items.push({id: '2024', content: '2024', group: '0Period', start: '2024-01-01', end: '2024-12-31', type: 'background', className: 'cls2024 negative'}) ;
+    items.push({id: '2025', content: '2025', group: '0Period', start: '2025-01-01', end: '2025-12-31', type: 'background', className: 'cls2025' }) ;
+    
+    // Configuration for the Timeline
+    let options = {
+    //   align: 'left',
+      start: '2017',
+      end: '2026',
+      groupOrder: function (a, b) {
+          return a.value - b.value;
+      },
+      type: 'point',
+      stack: true,
+      verticalScroll: true,
+      zoomKey: 'ctrlKey',
+      hiddenDates: [
+        // {start: '2020-01-01 00:00:00', end: '2020-12-31 23:59:59'},
+        //     {start: '2013-10-26 00:00:00', end: '2013-10-28 00:00:00', repeat: 'weekly'}, // daily weekly monthly yearly
+        //     {start: '2013-03-29 20:00:00', end: '2013-03-30 09:00:00', repeat: 'daily'} // daily weekly monthly yearly
+        ]
+    };
+    
+    let container_ = document.getElementById('visualization'); 
+    console.log(groups, items, container_)
+    
+    let timeline_ = new vis.Timeline(container_, items, groups, options);
+    console.log('timeline_', timeline_)
+    
+    return(timeline_)
 }
 
 function setTimeline(_id, dataset){
@@ -159,8 +223,16 @@ function prepNodes(measureNodes) {
 		let _content = measureNodes[i]["foafiaf:shortname"] || measureNodes[i]["rdfs:label"];
 		let _category = measureNodes[i]["foafiaf:categories"] || null;
 		
-		item.id = i;
-		item.group = _spoke.substring(_spoke.indexOf(":") + 1);
+		item.id = _id;
+		//item.group = _spoke.substring(_spoke.indexOf(":") + 1);
+		let _group = measureNodes[i]["foafiaf:Spoke"] || '';
+		if (_group) _group = _group.replace('foafiaf:', '') ;
+	    if (_group) _group = _group.replace('&', '') ;
+	    item.group = _group;
+	    
+	   // let _subgroup = null          use sungroup for sequence of milestones?
+	   // item.subgroup = _subgroup;
+	    
 		item.segment = _segment;
 		item.spoke = _spoke;
 		item.strategy = _strategy;
@@ -171,13 +243,13 @@ function prepNodes(measureNodes) {
 		item.content = _content;
 		
 		
-		console.log('_category', _category)
-		console.log('_content', _content)
-		console.log('_project', _project)
-		console.log('_status', _status)
-		console.log('_color', _color)
-		console.log('_start', _start)
-		console.log('_end', _end)
+// 		console.log('_category', _category)
+// 		console.log('_content', _content)
+// 		console.log('_project', _project)
+// 		console.log('_status', _status)
+// 		console.log('_color', _color)
+// 		console.log('_start', _start)
+// 		console.log('_end', _end)
 		item.title =  _category + ": " + _content + " " 
 		if (_project !== "")  item.title = item.title + "\n" + "Project: " + _project ;
 		if (_status !== "")  item.title =  item.title + "\n" + "Status: " + _status ;
@@ -188,10 +260,10 @@ function prepNodes(measureNodes) {
 		
 		item.category = _category;          // Output or Outcome??
 		if (item.category === 'Output') {
-		   item.className = 'output'
+		   item.className = 'output' + ' cls' + _start  // trim to only year?
 		}
 		if (item.category === 'Outcome') {
-		    item.className = 'outcome'
+		    item.className = 'outcome'+ ' cls' + _start
 		}
 		
 		item.start = _start;
@@ -200,7 +272,7 @@ function prepNodes(measureNodes) {
 // 		if (item.end) item.end = item.end + '-13-31' ;
 		
 		if (item.start) {
-		    //console.log('item', item)
+		    console.log('item', item)
 		    items.push(item)
 		
 		}
@@ -318,3 +390,4 @@ function prepNodes(measureNodes) {
     	//console.log('Nodes', Nodes)
     	return Nodes;
     }
+
