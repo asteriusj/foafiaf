0d6ffbf44f114686d7c943218395a55fa03fd6b2
asteriusj <jstewart@asteriusmedia.com>
2017-04-21 11:25:13 -0500

    Create partition.js

---
+++ public/projects/partition.js
@@ -0,0 +1,113 @@
+'use strict';
+function whenready() {
+    console.log('whenready')
+ 
+//   <script type="text/javascript">
+
+var w = 1120,
+    h = 600,
+    x = d3.scale.linear().range([0, w]),
+    y = d3.scale.linear().range([0, h]);
+
+var vis = d3.select("#body").append("div")
+    .attr("class", "chart")
+    .style("width", w + "px")
+    .style("height", h + "px")
+  .append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
+
+var partition = d3.layout.partition()
+    .value(function(d) { return d.size; });
+
+//d3.json("/json/flare.json", function(root) {
+//d3.json("/json/testpartition.json", function(root) {
+// d3.json("/json/testpartition.1.json", function(root) {
+
+//console.log(' call getWheelJS ')
+getWheelJS( function(err, root) {
+    
+    console.log('getWheelJS root',root)
+    let spliced = { "name": "Top 25 by 2025", "children": root }
+    console.log('spliced',spliced)
+    root = spliced                                                          //insert wheel into outer parent
+    // console.log('partition getWheelJSroot',JSON.stringify(root))
+    
+    var g = vis.selectAll("g")
+          .data(partition.nodes(root))
+        .enter().append("svg:g")
+          .attr("transform", function(d) { return "translate(" + x(d.y) + "," + y(d.x) + ")"; })
+          .on("click", click);
+    
+    var kx = w / root.dx,
+          ky = h / 1;
+    
+    g.append("svg:rect")
+          .attr("width", root.dy * kx)
+          .attr("height", function(d) { return d.dx * ky; })
+          .attr("class", function(d) { return d.children ? "parent" : "child"; });
+    
+    g.append("svg:text")
+          .attr("transform", transform)
+          .attr("dy", ".35em")
+          .style("opacity", function(d) { return d.dx * ky > 12 ? 1 : 0; })
+          .text(function(d) { return d.name; })
+          
+    g.append("title")
+                .text(function(t) { return getTitle(t)}) ;      
+          
+    
+    d3.select(window)
+          .on("click", function() { click(root); })
+    
+    // .onclick handler
+     function click(d) {
+        if (!d.children) return;
+    
+        kx = (d.y ? w - 40 : w) / (1 - d.y);
+        ky = h / d.dx;
+        x.domain([d.y, 1]).range([d.y ? 40 : 0, w]);
+        y.domain([d.x, d.x + d.dx]);
+    
+        var t = g.transition()
+            .duration(d3.event.altKey ? 7500 : 750)
+            .attr("transform", function(d) { return "translate(" + x(d.y) + "," + y(d.x) + ")"; });
+    
+        t.select("rect")
+            .attr("width", d.dy * kx)
+            .attr("height", function(d) { return d.dx * ky; });
+    
+        t.select("text")
+            .attr("transform", transform)
+            .style("opacity", function(d) { return d.dx * ky > 12 ? 1 : 0; });
+    
+        d3.event.stopPropagation();
+    }
+      
+    // add
+    // .ondblClick handler
+    function dblclick(d) {
+            console.log('dblclick', d)
+            let msg = getDetails(d)
+              window.confirm(msg)
+    }
+    // .contextmenu handler
+    function contextmenu(d) {
+              console.log('contextmenu', d)
+              let msg = getDetails(d)
+              //window.confirm(msg)
+              let comments = window.prompt(msg, "Comments:")
+              if (comments !== "Comments:") {
+                console.log('comments', comments)
+                sendComment(comments)
+              }
+    }
+        
+
+  function transform(d) {
+    return "translate(8," + d.dx * ky / 2 + ")";
+  }
+});
+
+    // </script>   
+}
